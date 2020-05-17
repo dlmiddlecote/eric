9f4ece13a1da006d00bef62b64ab8d9abbb81469
Daniel Middlecote <daniel@ometria.com>
2020-05-17 14:54:16 +0100

    Run in k8s

--- Makefile
+++ Makefile
@@ -1,3 +1,6 @@
+NAME   := eric
+MODULE := eric
+
 .PHONY: up
 up: install
 	docker-compose up -d
@@ -12,12 +15,36 @@ install:
 
 .PHONY: format
 format:
-	poetry run black -l 120 eric
+	poetry run black -l 120 $(MODULE)
 
-.PHONY: run
-run:
-	poetry run python eric/app.py worker -l info
+.PHONY: consumer
+consumer:
+	poetry run python $(MODULE)/consumer.py worker -l info
+
+.PHONY: producer
+producer:
+	poetry run python $(MODULE)/producer.py
 
 .PHONY: docker
 docker:
-	docker build -t eric:faust .
+	docker build -t $(NAME):faust .
+
+.PHONY: kind
+kind:
+	kind create cluster --name $(NAME) --image kindest/node:v1.14.10
+
+.PHONY: load-image
+load-image: docker
+	kind load docker-image $(NAME):faust --name $(NAME)
+
+.PHONY: k8s-kafka
+k8s-kafka:
+	kubectl --context kind-$(NAME) apply -R -f k8s/kafka
+
+.PHONY: k8s-consumer
+k8s-consumer:
+	kubectl --context kind-$(NAME) apply -f k8s/consumer.yaml
+
+.PHONY: k8s-producer
+k8s-producer:
+	kubectl --context kind-$(NAME) apply -f k8s/producer.yaml
--- eric/producer.py
+++ eric/producer.py
@@ -1,51 +1,38 @@
 import os
 import json
-from random import random
+import logging
+import time
+from random import randint
 from kafka import KafkaProducer
 
+logger = logging.getLogger(__name__)
+
 # Which Kafka should we connect to?
-KAFKA_BROKER = os.getenv("KAFKA_BROKER", default="kafka://localhost:9092")
-TOPIC = 'test'
-KEY = 'test'
+KAFKA_BROKER = os.getenv("KAFKA_BROKER", default="localhost:9092")
 
 
-def publish_message(producer_instance, topic_name, key, value):
-    try:
-        key_bytes = bytes(key, encoding='utf-8')
-        value_bytes = bytes(value, encoding='utf-8')
-        producer_instance.send(topic_name, key=key_bytes, value=value_bytes)
-        producer_instance.flush()
-        print('Message published successfully.')
-    except Exception as ex:
-        print('Exception in publishing message')
-        print(ex)
+def main():
+    kafka_producer = KafkaProducer(bootstrap_servers=KAFKA_BROKER)
 
-
-def connect_kafka_producer():
-    _producer = None
-    try:
-        # host.docker.internal is how a docker container connects to the local
-        # machine.
-        # Don't use in production, this only works with Docker for Mac in
-        # development
-        _producer = KafkaProducer(
-            bootstrap_servers=[KAFKA_BROKER])
-    except Exception as ex:
-        print('Exception while connecting Kafka')
-        print(ex)
-    return _producer
-
-
-if __name__ == '__main__':
-    kafka_producer = connect_kafka_producer()
-    index = 0
+    p = 0
     while True:
         message = {
-            'index': index % 8,
-            'name': 'Rincewind',
+            "index": randint(0, 10000),
+            "name": "Rincewind",
         }
-        publish_message(kafka_producer, TOPIC, KEY, json.dumps(message))
-        index += 1
 
-    if kafka_producer is not None:
-        kafka_producer.close()
+        key = bytes("key", encoding="utf-8")
+        value = bytes(json.dumps(message), encoding="utf-8")
+
+        kafka_producer.send("msgs", key=key, value=value)
+        kafka_producer.flush()
+
+        logger.info("Sent message.")
+
+        time.sleep(2)
+
+        p += 1
+
+
+if __name__ == "__main__":
+    main()
