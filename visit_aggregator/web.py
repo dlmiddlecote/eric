5479b0e725baa29dcc2d3c36b1527f2bf2ba4d35
Daniel Middlecote <daniel@ometria.com>
2020-05-20 13:05:12 +0100

    Add ability to filter accounts in websocket

--- visit_aggregator/web.py
+++ visit_aggregator/web.py
@@ -1,3 +1,5 @@
+import json
+import logging
 from collections import defaultdict
 from typing import List, MutableMapping, Set
 
@@ -10,12 +12,20 @@ from faust import web
 from faust.events import Event
 from faust.types.tuples import TP
 
+logger: logging.Logger = logging.getLogger("visit_aggregator")
+
 
 @app.page("/ws")
 async def websocket_handler(self, request):
     """
     Push new active visitor counts to websocket.
+
+    Accepts '{"type": "setFilter", "filter": {"account": <id>}}' payload to
+    filter out account messages sent on connection.
     """
+
+    # Capture account to send events for.
+    account_id = None
 
     # Prepare request for websocket
     ws = aiohttp.web.WebSocketResponse()
@@ -27,19 +37,29 @@ async def websocket_handler(self, request):
         async for event in s:
             if isinstance(event, Event):
                 av = event.value
-                await ws.send_json(
-                    {
-                        "account_id": av.account_id,
-                        "store_id": av.store_id,
-                        "ts_from": av.window[0],
-                        "ts_to": av.window[1],
-                        "count": av.count,
-                    }
-                )
+                if (not account_id) or (account_id == av.account_id):
+                    await ws.send_json(
+                        {
+                            "account_id": av.account_id,
+                            "store_id": av.store_id,
+                            "ts_from": av.window[0],
+                            "ts_to": av.window[1],
+                            "count": av.count,
+                        }
+                    )
 
             elif event.type == aiohttp.WSMsgType.TEXT:
                 if event.data == "close":
                     await ws.close()
+                else:
+                    try:
+                        data = json.loads(event.data)
+                        if data.get("type") == "setFilter":
+                            account_id = data["filter"]["account"]
+                            logger.info(f"Sending messages for account {account_id}")
+
+                    except Exception as e:
+                        logger.exception(f"Error handling message '{event.data}': {str(e)}")
 
             elif event.type == aiohttp.WSMsgType.ERROR:
                 logger.warning(f"ws connection closed with error: {ws.exception()}")
